9a0d0f244e9f3f7198d5912a3ae5af8855d37ed5
Jacob Batterman <ctrjcb@gmail.com>
2019-12-03 23:11:49 -0700

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+import Cookie from 'js-cookie';
+
+import Login from './login';
+
+export default function App () {
+  const [name, setName] = useState (Cookie.get ('jsCookieDemo'));
+
+  const handleSet = () => {
+    setName (Cookie.get ('jsCookieDemo'));
+  };
+
+  const handleDelete = () => {
+    Cookie.remove ('jsCookieDemo');
+    setName (Cookie.get ('jsCookieDemo'));
+  };
+  return (
+    <div className="app">
+      {Cookie.get ('jsCookieDemo')
+        ? <div>
+            <p>Welcome, {name}</p>
+            <button onClick={handleDelete}>Log Out</button>
+          </div>
+        : <Login handleSet={handleSet} />}
+    </div>
+  );
+}
